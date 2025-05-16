9f340ce817f9bebb4d61ff1a8a339d49b88a10dc
Sakhawat Hossain <hsakhawat135@gmail.com>
2025-05-17 05:07:54 +0600

    serializers update

---
+++ apps/auth/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'email', 'first_name', 'last_name', 'password']  # Include fields you want to expose
+        extra_kwargs = {'password': {'write_only': True}}  # Password should not be readable
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = CustomUser(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)  # Password update is optional
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
